7ae841f66c8ef341aa4718a0b27177dd08a6a13f
adarsh <adarshshukla0627@gmail.com>
2026-07-31 16:24:45 +0530

    Initial commit

---
+++ src/components/dashboard/Sidebar/index.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+import CategoryInfo from "./CategoryInfo";
+import Categories from "./Categories";
+
+const SidebarWrapper = styled.div`
+  display: grid;
+  position: fixed;
+  left: 0;
+  grid-template-rows: 30vh 60vh 10vh;
+  width: 20vw;
+  .footer {
+    display: flex;
+    padding-left: 3vh;
+    font-size: 1.1vw;
+    color: var(--theme-grey);
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 16px;
+    a{width: 50%}
+  }
+`;
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <SidebarWrapper>
+        <CategoryInfo slug={this.props.slug} color={this.props.color} />
+        <Categories slug={this.props.slug} url={this.props.url} color={this.props.color} />
+        <div className="footer">© 2025 ProjectLearn</div>
+      </SidebarWrapper>
+    );
+  }
+}
---
+++ src/components/landing/Footer.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  margin: 0vh 5vw;
+  display: flex;
+  padding: 3vh;
+  font-size: 1.3vw;
+  color: var(--themeTextSecondaryDark);
+  a{
+    display: flex;
+    align-items: center;
+    }
+
+  .footer-sponsor{
+    display: flex;
+    align-items: center;
+    gap: 16px;
+  }
+  .footer-container {
+    margin-left: auto;
+    display: flex;
+    a{
+    display: flex;
+    align-items: center;
+    }
+  }
+  .footer-content {
+    margin-right: 2rem;
+    color: var(--themeTextSecondaryDark);
+    
+  }
+  .img-link{
+    width: 11.5%;
+  }
+  @media only screen and (min-width: 320px) and (max-width: 480px) {
+    margin: 0vh 1vw;
+    font-size: 4vw;
+    flex-direction: column;
+    gap: 16px;
+    align-items: center;
+
+    .footer-container {
+      display: none;
+    }
+
+    .img-link{
+      width: 50%;
+    }
+    .mobile {
+      display: none;
+    }
+    .img-link{
+      display: flex;
+      justify-content: center;
+    }
+    .footer-content {
+      dis
+      margin-right: 0rem;
+      margin-left: 3.5vw;
+    }
+  }
+`;
+
+export default function Footer() {
+  return (
+    <FooterWrapper>
+      <div className="footer-sponsor">© 2025 ProjectLearn</div>
+
+      <div className="footer-container">
+        <a
+          href="https://github.com/adarshshukla112233445566-ux/skillforge"
+          className="footer-content"
+          target="_blank"
+        >
+          About
+        </a>
+        <a
+          href="https://github.com/adarshshukla112233445566-ux/skillforge"
+          className="footer-content mobile"
+          target="_blank"
+        >
+          Contribution Guidelines
+        </a>
+        {/* <a
+          href="https://opencollective.com/projectlearn"
+          className="footer-content"
+          target="_blank"
+        >
+          Donate
+        </a> */}
+
+      </div>
+    </FooterWrapper>
+  );
+}
